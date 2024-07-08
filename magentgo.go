d9148bdc49f899520b515eac127ac3b19866e02a
ethan <ethan@thepixel.com>
2024-07-08 21:47:38 +0100

    Re-factor main call method.

--- magentgo.go
+++ magentgo.go
@@ -133,31 +133,23 @@ func (c *Client) setVersion(version int) error {
 
 // make http request from the client. Attempts to marshal against type struct, returns raw result as byte slice
 func (c *Client) call(endpoint string, httpVerb string, bodyType any, responseType any, ctx context.Context) ([]byte, error) {
-	var marshalled []byte
 	var err error
+
 	requestUrl := fmt.Sprintf("%s%s", c.apiBaseUrl, endpoint)
 
 	fmt.Println(requestUrl)
 
-	str, ok := bodyType.(string)
-	if ok {
-		marshalled = []byte(str)
-	}
-
-	if len(marshalled) == 0 {
-		marshalled, err = json.Marshal(&bodyType)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	req, err := http.NewRequestWithContext(ctx, httpVerb, requestUrl, bytes.NewReader(marshalled))
+	body, err := c.processBodyType(bodyType)
 	if err != nil {
 		return nil, err
 	}
 
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+c.bearerToken)
+	req, err := c.newRequest(ctx, httpVerb, requestUrl, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+
+	c.setRequestHeaders(req, c.bearerToken)
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -165,7 +157,7 @@ func (c *Client) call(endpoint string, httpVerb string, bodyType any, responseTy
 	}
 	defer res.Body.Close()
 
-	body, err := io.ReadAll(res.Body)
+	body, err = io.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
 	}
@@ -178,4 +170,41 @@ func (c *Client) call(endpoint string, httpVerb string, bodyType any, responseTy
 	}
 
 	return body, nil
+}
+
+// type assertions to handle body type, converts whichever type provided into a byte slice
+func (c *Client) processBodyType(body any) ([]byte, error) {
+	var marshalled []byte
+	var err error
+	switch v := body.(type) {
+	case string:
+		marshalled = []byte(v)
+	case []byte:
+		marshalled = v
+	default:
+		marshalled, err = json.Marshal(&body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return marshalled, nil
+}
+
+// create new request with context to be used in main call function
+func (c *Client) newRequest(ctx context.Context, verb string, url string, reader *bytes.Reader) (*http.Request, error) {
+	req, err := http.NewRequestWithContext(ctx, verb, url, reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+// implement request headers
+func (c *Client) setRequestHeaders(req *http.Request, token string) *Client {
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer " + token)
+
+	return c
 }
